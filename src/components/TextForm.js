5c4b26ad97caa8243f87de843328ac69010afba3
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2022-12-27 02:53:51 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-export default function TextForm() {
+export default function TextForm(props) {
     const [text, setText] = useState("")
 
     const handleOnChange = (e) => setText(e.target.value);
@@ -25,9 +25,9 @@ export default function TextForm() {
     const copyText = () => navigator.clipboard.writeText(text);
 
     return (
-        <div className='container'>
+        <div className='container'  style={{backgroundColor:props.mode === "light"?"white":"#042743",color:props.mode === "light"?"#042743":"white"}}>
             <h1 className='my-3'>Enter text to analyze</h1>
-            <div className="my-3"><textarea value={text} onChange={handleOnChange} className="form-control" id="myBox" rows="8"></textarea></div>
+            <div className="my-3"><textarea value={text} onChange={handleOnChange} className="form-control" style={{backgroundColor:props.mode === "light"?"white":"#13466e",color:props.mode === "light"?"black":"white"}} id="myBox" rows="8"></textarea></div>
             <button disabled={text.length === 0} type="button" className="btn btn-primary m-1" onClick={toUppercase}>Convert to UpperCase</button>
             <button disabled={text.length === 0} type="button" className="btn btn-primary m-1" onClick={toLowercase}>Convert to LowerCase</button>
             <button disabled={text.length === 0} type="button" className="btn btn-primary m-1" onClick={toCamelCase}>Convert to CamelCase</button>
@@ -38,9 +38,10 @@ export default function TextForm() {
 
             <div className="container my-3">
                 <h3>Your Text Info</h3>
-                <p>{text.split(/\s+/).filter(element=>{return element.length!==0}).length} words and {text.length} characters</p>
+                <p style={{color:"#857f7f"}}>{text.split(/\s+/).filter(element=>{return element.length!==0}).length} words and {text.length} characters</p>
+                <p style={{color:"#857f7f"}}>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to read</p>
                 <h3>Preview</h3>
-                <p>{text.length === 0? "Write Something to preview":text}</p>
+                <p style={{color:"#857f7f"}}>{text.length === 0? "Write Something to preview":text}</p>
             </div>
         </div>
     )
