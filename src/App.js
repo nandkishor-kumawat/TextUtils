b8a9e50b355de2000fffca3a90092e181ab8a175
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2022-12-27 02:53:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState("light");
+  
+  const toggleMode = () =>{
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = '#042743'
+    }else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  }
+  
   return (
     <BrowserRouter>
       <div className="App">
-        <Navbar heading="TextUtils"/>
+        <Navbar heading="TextUtils" mode={mode} toggleMode={toggleMode}  />
         <Routes>
-          <Route path="/" element={<TextForm/>} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<TextForm mode={mode}/>} />
+          <Route path="/about" element={<About mode={mode} />} />
         </Routes>
       </div>
     </BrowserRouter>
